fa5d7b2c57199c7c0d75f79039edf39f00519ef9
Henry Cappel <henry@evevehicles.com>
2023-01-04 12:49:37 -0600

    fixed client

--- src/server_connection_client.py
+++ src/server_connection_client.py
@@ -10,14 +10,14 @@ def serviceCall():
 	rospy.wait_for_service('check_ups')
 	try:
 		# Call the service on the pi to return connection status
-		connection_checkups = rospy.ServiceProxy('check_ups', checkups)
+		connection_checkups = rospy.ServiceProxy('check_ups', ui_checkups_msg)
 		result = connection_checkups(True)
 
 		#Publish the ping for the UI to see
-		checkups = ui_checkups_msg()
-		checkups.pi_connect = result.pi_connect
-		checkups.px4_connect = result.px4_connect
-		checkups_pub.publish(checkups)
+		checkups_msg = ui_checkups_msg()
+		checkups_msg.pi_connect = result.pi_connect
+		checkups_msg.px4_connect = result.px4_connect
+		checkups_pub.publish(checkups_msg)
 
 	except rospy.ServiceException as e:
 		print("Service call failed: %s"%e)
